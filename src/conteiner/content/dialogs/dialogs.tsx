a9937b3acfb79e6bef168031c5c214dd603d0c35
Sasha <skalitsenia@mail.ru>
2023-06-04 00:04:59 +0300

    BrowserRouter-Navlink

--- src/conteiner/content/dialogs/dialogs.tsx
+++ src/conteiner/content/dialogs/dialogs.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import d from "./dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
-
-export const Dialods = function (props : any) {
+export const Dialods = function (props: any) {
     return (
-        <div>
-            Dialogs
+        <div className={d.dialogs}>
+            <div className={d.dialogsItem}>
+                <div className={d.dialog}><NavLink to={'/dialogs/1'}>Masha</NavLink></div>
+                <div className={d.dialog}><NavLink to={'/dialogs/2'}>Dasha</NavLink></div>
+                <div className={d.dialog}><NavLink to={'/dialogs/3'}>Sasha</NavLink></div>
+                <div className={d.dialog}><NavLink to={'/dialogs/4'}>Sveta</NavLink></div>
+                <div className={d.dialog}><NavLink to={'/dialogs/5'}>Nastya</NavLink></div>
+            </div>
+            <div className={d.messangesItem}>
+                <div className={d.messange}>Hello</div>
+                <div className={d.messange}>Bye</div>
+                <div className={d.messange}>How are you?</div>
+                <div className={d.messange}>Good</div>
+                <div className={d.messange}>Go to bed</div>
+            </div>
         </div>
     )
 }
