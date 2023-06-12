0886d81f0bef2b9a1b79f5b0dcbb302997d6113d
Sasha <skalitsenia@mail.ru>
2023-06-12 17:18:26 +0300

    Night layout

--- src/conteiner/content/dialogs/dialogs.tsx
+++ src/conteiner/content/dialogs/dialogs.tsx
@@ -1,15 +1,29 @@
 import React, {useState} from "react";
 import d from "./dialogs.module.css"
 import {DilogsComponent} from "./dialogs-components";
+import {v1} from "uuid";
 
+type DialogsType={
+    name: string
+    id: string
+}
 
 
 export const Dialods = function () {
 
+
+    const [message, setMessage] = useState<Array<DialogsType>>([
+    {name: "Masha" , id : v1()},
+    {name: "Dasha" , id : v1()},
+    {name: "Sasha" , id :v1()},
+    {name: "Eva" , id : v1()},
+    {name: "Nastya" , id : v1()}
+])
+
     return (
         <div className={d.dialogs}>
             <div className={d.dialogsItem}>
-              <DilogsComponent/>
+              <DilogsComponent messasge={message}/>
             </div>
             <div className={d.messangesItem}>
                 <div className={d.messange}>Hello</div>
