991b50c44282cdd45f3ae12ba730d31446a108ad
Sasha <skalitsenia@mail.ru>
2023-06-12 16:52:46 +0300

    Night layout

--- src/conteiner/content/dialogs/dialogs.tsx
+++ src/conteiner/content/dialogs/dialogs.tsx
@@ -1,30 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import d from "./dialogs.module.css"
-import {NavLink} from "react-router-dom";
-
-type DialogsType = {
-    name: string
-    id: number
-
-}
+import {DilogsComponent} from "./dialogs-components";
 
 
-const DilogsComponent = (props: DialogsType) => {
-    debugger
-    return (
-        <div className={d.dialog}><NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink></div>
-    )
-}
+
 export const Dialods = function () {
-    debugger
+
     return (
         <div className={d.dialogs}>
             <div className={d.dialogsItem}>
-                <DilogsComponent id={1} name={"Masha"}/>
-                <DilogsComponent id={2} name={"Dasha"}/>
-                <DilogsComponent id={3} name={"Sasha"}/>
-                <DilogsComponent id={4} name={"Alina"}/>
-                <DilogsComponent id={5} name={"Arina"}/>
+              <DilogsComponent/>
             </div>
             <div className={d.messangesItem}>
                 <div className={d.messange}>Hello</div>
