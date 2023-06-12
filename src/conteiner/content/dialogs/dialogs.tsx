8e54d933965fb62aecf2d2b7350a86071722f1f0
Sasha <skalitsenia@mail.ru>
2023-06-12 19:14:22 +0300

    Night layout

--- src/conteiner/content/dialogs/dialogs.tsx
+++ src/conteiner/content/dialogs/dialogs.tsx
@@ -1,36 +1,44 @@
 import React, {useState} from "react";
 import d from "./dialogs.module.css"
-import {DilogsComponent} from "./dialogs-components";
+import {DilogsComponent} from "./dialodsName/dialogs-components";
 import {v1} from "uuid";
+import {MessangesValue} from "./messanges-value/messanges-value";
 
-type DialogsType={
+type DialogsType = {
     name: string
     id: string
 }
-
+type messangesValueType= {
+    value : string
+    id : string
+}
 
 export const Dialods = function () {
 
 
-    const [message, setMessage] = useState<Array<DialogsType>>([
-    {name: "Masha" , id : v1()},
-    {name: "Dasha" , id : v1()},
-    {name: "Sasha" , id :v1()},
-    {name: "Eva" , id : v1()},
-    {name: "Nastya" , id : v1()}
-])
+    const [dialodsName, setMessage] = useState<Array<DialogsType>>([
+        {name: "Masha", id: v1()},
+        {name: "Dasha", id: v1()},
+        {name: "Sasha", id: v1()},
+        {name: "Eva", id: v1()},
+        {name: "Nastya", id: v1()}
+    ])
+
+    const [messangesValue, setMassangesValue] = useState<Array<messangesValueType>>([
+        {value: 'Hello',id: v1()},
+        {value: 'Bye',id: v1()},
+        {value: 'Good',id: v1()},
+        {value: 'Yo',id: v1()},
+        {value: 'Thank you',id: v1()}
+    ])
 
     return (
         <div className={d.dialogs}>
             <div className={d.dialogsItem}>
-              <DilogsComponent messasge={message}/>
+                <DilogsComponent dialodsName={dialodsName}/>
             </div>
-            <div className={d.messangesItem}>
-                <div className={d.messange}>Hello</div>
-                <div className={d.messange}>Bye</div>
-                <div className={d.messange}>How are you?</div>
-                <div className={d.messange}>Good</div>
-                <div className={d.messange}>Go to bed</div>
+            <div>
+                <MessangesValue messangesValue={messangesValue}/>
             </div>
         </div>
     )
