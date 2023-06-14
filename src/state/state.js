02a7027a3773c9adc4c77b13fca8d6cb735e5d27
Sasha <skalitsenia@mail.ru>
2023-06-15 00:36:36 +0300

    create state

--- src/state/state.js
+++ src/state/state.js
@@ -2,10 +2,13 @@ import {v1} from "uuid";
 
 
 const state = {
-    profilePage : {
-        posts : [
+    profilePage: {
+        posts: [
             {img: 'https://variety.com/wp-content/uploads/2022/12/MCDAVTH_WD063.jpg', comment: 'My first post'},
-            {img: 'https://www.soyuz.ru/public/uploads/files/2/7615320/202212191629123ffc8dd5e1.jpg', comment: 'My first post'},
+            {
+                img: 'https://www.soyuz.ru/public/uploads/files/2/7615320/202212191629123ffc8dd5e1.jpg',
+                comment: 'My first post'
+            },
             {img: 'https://static.riafan.ru/upload/images/2022/11/2/1045150_full.jpeg', comment: 'My first post'},
             {
                 img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyRvLjFK4n05DP0OuzXiZA8fOPQ2B_Ha9XLKHRCIcgWV8P4J3KU2Q52nIH9huW_K8EhOI&usqp=CAU',
@@ -13,15 +16,15 @@ const state = {
             },
         ]
     },
-    dialogsPage : {
+    dialogsPage: {
         dialogsName: [
-            {name: "Masha", id: v1()},
-            {name: "Dasha", id: v1()},
-            {name: "Sasha", id: v1()},
-            {name: "Eva", id: v1()},
-            {name: "Nastya", id: v1()}
+            {name: "Masha", id: v1(), src : 'https://pbs.twimg.com/media/Fk7QddhakAACAhj.jpg:large'},
+            {name: "Dasha", id: v1(), src : 'https://variety.com/wp-content/uploads/2023/03/Screen-Shot-2023-03-07-at-9.44.48-AM.png'},
+            {name: "Sasha", id: v1(), src : 'https://media.glamour.com/photos/637d12aa28410df29dcde4ab/1:1/w_844,h_844,c_limit/Wednesday_S1_E1_00_02_28_14R.jpg'},
+            {name: "Eva", id: v1(), src : 'https://www.naturaldiamonds.com/wp-content/uploads/2022/12/STI_TRE_Wednesday-Gothic-Jewelry_Jenna-Ortega_Instagram-mrenriquemelendez_IMG_4x5.jpg'},
+            {name: "Nastya", id: v1(), src : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZY63GS-0hxsII1vruGHI8czytThilOb7JQw&usqp=CAU'}
         ],
-        messages : [
+        messages: [
             {value: 'Hello', id: v1()},
             {value: 'Bye', id: v1()},
             {value: 'Good', id: v1()},
