077c00a158808aee5453691c1ce03117ae311ab6
Sasha <skalitsenia@mail.ru>
2023-06-14 19:35:42 +0300

    create state

--- src/conteiner/content/dialogs/dialogs.tsx
+++ src/conteiner/content/dialogs/dialogs.tsx
@@ -2,35 +2,23 @@ import React from "react";
 import d from "./dialogs.module.css"
 import {MessangesValue} from "./messanges-value/messanges-value";
 import {DialogsComponent} from "./dialodsName/dialogs-components";
-
-// type dialogsPageType = {
-//     dialogs : {
-//         dialogsName: Array<DialogsType>
-//         messanges: Array<messangesValueType>
-//     }
-// }
-//
-// type DialogsType = {
-//     name: string
-//     id: string
-// }
-// type messangesValueType = {
-//     value: string
-//     id: string
-// }
-
 type propsType = {
     messages: Array<messagesType>
+    dialogsName : Array<dialogsNameType>
 }
 type messagesType = {
     value: string
     id: string
 }
+type dialogsNameType = {
+    name: string
+    id : string
+}
 export const Dialods = function (props: propsType) {
     return (
         <div className={d.dialogs}>
             <div className={d.dialogsItem}>
-                {/*<DialogsComponent dialodsName={props.dialogs.dialogsName}/>*/}
+                <DialogsComponent dialodsName={props.dialogsName}/>
             </div>
             <div>
                 <MessangesValue messangesValue={props.messages}/>
