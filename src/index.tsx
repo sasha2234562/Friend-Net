68519e26863a5aaf7f9ac158e4485ae090d8eb41
Sasha <skalitsenia@mail.ru>
2023-06-24 23:37:34 +0300

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import state from "./state/state";
-import {BrowserRouter} from "react-router-dom";
-import {addPost} from "./state/state";
 import {render} from "./render";
 
 render()
