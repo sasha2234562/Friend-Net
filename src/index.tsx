407e5b485cac7a3e0132ad7ac4ef8a74acfcf474
Sasha <skalitsenia@mail.ru>
2023-06-25 19:45:07 +0300

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,28 @@
-import './index.css';
-import {render} from "./render";
+// import './index.css';
+// import {render} from "./render";
+//
+// render()
 
-render()
+
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import {addPost, changeNewpostText, subscribe} from "./state/state";
+import state from "./state/state";
+
+
+export const rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App
+                state={state}
+                addPost={addPost}
+                changeNewpostText={changeNewpostText}
+            />
+        </BrowserRouter>,
+        document.getElementById('root')
+    )
+}
+rerenderEntireTree()
+
+subscribe(rerenderEntireTree)
