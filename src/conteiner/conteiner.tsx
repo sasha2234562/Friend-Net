b66d67a94f2bb783ff4e824f71a1777c0455d817
Sasha <skalitsenia@mail.ru>
2023-07-04 22:12:39 +0300

    rename store

--- src/conteiner/conteiner.tsx
+++ src/conteiner/conteiner.tsx
@@ -1,15 +1,17 @@
 import Header from "./header/header-bacground";
 import Content from "./content/content";
 import {stateType} from "../App";
-// import {addPost, changeNewpostText} from "../state/state";
 
 
-function Conteiner(props : stateType) {
+function Conteiner(props: stateType) {
 
     return (
         <div>
             <Header/>
-            <Content changeNewpostText={props.changeNewpostText} state={props.state} addPost={props.addPost}/>
+            <Content
+                // changeNewpostText={props.changeNewpostText}
+                state={props.state}
+                dispatch={props.dispatch}/>
         </div>
     )
 }
