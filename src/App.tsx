a18bda7dce3b92c7859e17769b9fd99b43314cfd
Sasha <skalitsenia@mail.ru>
2023-07-22 22:19:59 +0300

    creat api context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import Conteiner from "./conteiner/conteiner";
 import c from "./conteiner/conteiner.module.css"
-import {storeType} from "./index";
 
 
-
-function App(props: storeType) {
+function App() {
 
     return (
         <div className={c.conteiner}>
-            <Conteiner
-                state={props.state}
-                dispatch={props.dispatch}/>
+            <Conteiner/>
         </div>
     );
 }
