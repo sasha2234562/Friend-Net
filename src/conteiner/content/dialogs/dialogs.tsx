c70a54978936c733adc7423e62f7cde0e671bfa3
Sasha <skalitsenia@mail.ru>
2023-07-22 22:19:45 +0300

    creat api context

--- src/conteiner/content/dialogs/dialogs.tsx
+++ src/conteiner/content/dialogs/dialogs.tsx
@@ -1,34 +1,21 @@
 import React from "react";
 import d from "./dialogs.module.css"
 import {DialogsComponent} from "./dialodsName/dialogs-components";
-import {storeType} from "../../../index";
 import {ConteinerMessangesValue} from "./messanges-value/conteiner-messanges-value";
 import StoreContext from "../../../store-context";
 
-// export type dialogsType = {
-//     dialogsPage: {
-//         messages: Array<messagesType>
-//         dialogsName: Array<DialogsNameType>
-//         newMessage : string
-//     }
-//     dispatch: (action: ActionType) => void
-// }
-// export type messagesType = {
-//     value: string
-//     id: string
-// }
 
-
-export const Dialods = function (props: storeType) {
+export const Dialods = function () {
     return <StoreContext.Consumer>{
         (store) => <div className={d.dialogs}>
             <div className={d.dialogsItem}>
-                <DialogsComponent dialodsName={props.state.dialogsPage.dialogsName}/>
+                <DialogsComponent
+                    dialodsName={store.getState().dialogsPage.dialogsName}/>
             </div>
             <div>
                 <ConteinerMessangesValue
-                    state={props.state}
-                    dispatch={props.dispatch}
+                    state={store.getState()}
+                    dispatch={store.dispatch}
                 />
             </div>
         </div>
