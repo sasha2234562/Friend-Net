72b87cc8f8987c67a38c071d3fe53738edcb8b85
Sasha <skalitsenia@mail.ru>
2023-08-08 22:34:58 +0300

    create users

--- src/redux/user-reduser.ts
+++ src/redux/user-reduser.ts
@@ -1,36 +1,93 @@
 import {v1} from "uuid";
+import {AddMessageAC, changeMessageAC} from "./message-reduser";
 
-const users = [
-    {
-        id: v1(), name: 'Sasha', followed: true, status: 'I\'am man ',
-        location: {
-            cityName: 'Hanoi',
-            country: 'Vietnam'
+const FOLLOW = 'FOLLOW'
+const UNFOLLOW = 'UNFOLLOW'
+const SET_USERS = 'SET-USERS'
+
+const initialState = {
+    users: [
+        {
+            id: v1(), name: 'Sasha', followed: true, status: 'I\'am man ',
+            location: {
+                cityName: 'Hanoi',
+                country: 'Vietnam'
+            },
+            photo: 'https://assets.gq.ru/photos/602b778f6fc740d3f63c4faa/1:1/w_1600%2Cc_limit/120121_Anime_03.jpg'
         },
-        photo: 'https://assets.gq.ru/photos/602b778f6fc740d3f63c4faa/1:1/w_1600%2Cc_limit/120121_Anime_03.jpg'
-    },
-    {
-        id: v1(), name: 'Dasha', followed: true, status: 'I\'am women ',
-        location: {
-            cityName: 'Vitebsk',
-            country: 'Belarus'
+        {
+            id: v1(), name: 'Dasha', followed: true, status: 'I\'am women ',
+            location: {
+                cityName: 'Vitebsk',
+                country: 'Belarus'
+            },
+            photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM5yQJfYX072PTGOv1rcPqmFxctGijlCL0MQ&usqp=CAU'
         },
-        photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM5yQJfYX072PTGOv1rcPqmFxctGijlCL0MQ&usqp=CAU'
-    },
-    {
-        id: v1(), name: 'Masha', followed: false, status: 'I\'am women ',
-        location: {
-            cityName: 'Minsk',
-            country: 'Belarus'
+        {
+            id: v1(), name: 'Masha', followed: false, status: 'I\'am women ',
+            location: {
+                cityName: 'Minsk',
+                country: 'Belarus'
+            },
+            photo: 'https://img.freepik.com/free-photo/the-girl-in-the-sky_1340-27755.jpg'
         },
-        photo: 'https://img.freepik.com/free-photo/the-girl-in-the-sky_1340-27755.jpg'
-    },
-    {
-        id: v1(), name: 'Alex', followed: false, status: 'I\'am man ',
-        location: {
-            cityName: 'Moscow',
-            country: 'Russia'
-        },
-        photo: 'https://avatars.mds.yandex.net/i?id=1734dc3ff9417d6092956f5da41811a1-4416220-images-thumbs&n=13'
+        {
+            id: v1(), name: 'Alex', followed: false, status: 'I\'am man ',
+            location: {
+                cityName: 'Moscow',
+                country: 'Russia'
+            },
+            photo: 'https://avatars.mds.yandex.net/i?id=1734dc3ff9417d6092956f5da41811a1-4416220-images-thumbs&n=13'
+        }
+    ]
+}
+
+
+const usersReduser = (state = initialState, action: setUsersACType) => {
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(item => {
+                    return item.id === action.userID ? item.followed = true : item
+                })
+            }
+
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(item=> {
+                    return item.followed = false
+                })
+            }
+
+        case SET_USERS:
     }
-]
+}
+
+export const followAC = (userID) => {
+    return {
+        type: FOLLOW,
+        userID
+    }
+}
+
+export const unFollowAC = (userID) => {
+    return {
+        type: UNFOLLOW,
+        userID
+    }
+}
+
+export const setUsersAC = (userID) => {
+    return {
+        type: SET_USERS,
+        userID
+    }
+}
+
+type followACType = ReturnType<typeof followAC>
+type unFollowACType = ReturnType<typeof unFollowAC>
+type setUsersACType = ReturnType<typeof setUsersAC>
+
+type AllType = followACType | unFollowACType | setUsersACType
