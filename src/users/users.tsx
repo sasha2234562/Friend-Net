df7a03dae76ae7bbc1421a449401375f23180f73
Sasha <skalitsenia@mail.ru>
2023-08-09 22:30:18 +0300

    create users

--- src/users/users.tsx
+++ src/users/users.tsx
@@ -25,7 +25,8 @@ export const Users = (props: MapStateToPropsUsers) => {
                     <img src={item.photo}/>
                     <button>{item.followed}</button>
                     <span>{item.status}</span>
-
+                    <span>{item.location.country}</span>
+                    <span>{item.location.cityName}</span>
                 </div>
             })}
         </div>
