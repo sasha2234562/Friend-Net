ec248c508865b06ef88daa1d9cf541b48fda6821
Sasha <skalitsenia@mail.ru>
2023-08-12 17:17:26 +0300

    create users errors

--- src/conteiner/content/well/well-beach/beach.tsx
+++ src/conteiner/content/well/well-beach/beach.tsx
@@ -3,7 +3,8 @@ import  b from  "./beach.module.css";
 function Beach () {
     return (
         <div className={b.beach}>
-            <img src={'https://i.ytimg.com/vi/BNTs6-pNFRk/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-DoACuAiKAgwIABABGBMgTyh_MA8=&rs=AOn4CLBtSoyoeyqJI2TZqmhDkCrwZRgNww'}/>
+
+            <img alt={'delete photo'} src={'https://i.ytimg.com/vi/BNTs6-pNFRk/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-DoACuAiKAgwIABABGBMgTyh_MA8=&rs=AOn4CLBtSoyoeyqJI2TZqmhDkCrwZRgNww'}/>
         </div>
     )
 }
