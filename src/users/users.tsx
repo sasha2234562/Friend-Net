3ce3aa6755684cc58269cec2c27130c45a02ebd7
Sasha <skalitsenia@mail.ru>
2023-08-12 21:53:47 +0300

    user server requests

--- src/users/users.tsx
+++ src/users/users.tsx
@@ -8,12 +8,16 @@ export type UserType = {
     id: string
     name: string,
     followed: boolean,
-    status: string,
+    status: string | null,
     location: {
         cityName: string,
         country: string
     },
-    photo: string
+    photo: {
+        large: null
+        small: null
+    },
+    uniqueUrlName: null
 
 }
 type StatePropsType = MapStateToPropsUsersType & MapDispatchToPropsType
@@ -31,51 +35,52 @@ type MapDispatchToPropsType = {
 export const Users = (props: StatePropsType) => {
     if (props.users.length === 0) {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => console.log(response))
+            .then(response => //props.setUsers(response.data.items) )
+                console.log(response.data.items))
     }
 
-    useEffect(() => {
-        props.setUsers([
-            {
-                id: v1(), name: 'Sasha', followed: true, status: 'I\'am man ',
-                location: {
-                    cityName: 'Hanoi',
-                    country: 'Vietnam'
-                },
-                photo: 'https://assets.gq.ru/photos/602b778f6fc740d3f63c4faa/1:1/w_1600%2Cc_limit/120121_Anime_03.jpg'
-            },
-            {
-                id: v1(), name: 'Dasha', followed: true, status: 'I\'am women ',
-                location: {
-                    cityName: 'Vitebsk',
-                    country: 'Belarus'
-                },
-                photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM5yQJfYX072PTGOv1rcPqmFxctGijlCL0MQ&usqp=CAU'
-            },
-            {
-                id: v1(), name: 'Masha', followed: false, status: 'I\'am women ',
-                location: {
-                    cityName: 'Minsk',
-                    country: 'Belarus'
-                },
-                photo: 'https://img.freepik.com/free-photo/the-girl-in-the-sky_1340-27755.jpg'
-            },
-            {
-                id: v1(), name: 'Alex', followed: false, status: 'I\'am man ',
-                location: {
-                    cityName: 'Moscow',
-                    country: 'Russia'
-                },
-                photo: 'https://avatars.mds.yandex.net/i?id=1734dc3ff9417d6092956f5da41811a1-4416220-images-thumbs&n=13'
-            }
-        ])
-    }, [])
+    // useEffect(() => {
+    //     props.setUsers([
+    //         {
+    //             id: v1(), name: 'Sasha', followed: true, status: 'I\'am man ',
+    //             location: {
+    //                 cityName: 'Hanoi',
+    //                 country: 'Vietnam'
+    //             },
+    //             photo: 'https://assets.gq.ru/photos/602b778f6fc740d3f63c4faa/1:1/w_1600%2Cc_limit/120121_Anime_03.jpg'
+    //         },
+    //         {
+    //             id: v1(), name: 'Dasha', followed: true, status: 'I\'am women ',
+    //             location: {
+    //                 cityName: 'Vitebsk',
+    //                 country: 'Belarus'
+    //             },
+    //             photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM5yQJfYX072PTGOv1rcPqmFxctGijlCL0MQ&usqp=CAU'
+    //         },
+    //         {
+    //             id: v1(), name: 'Masha', followed: false, status: 'I\'am women ',
+    //             location: {
+    //                 cityName: 'Minsk',
+    //                 country: 'Belarus'
+    //             },
+    //             photo: 'https://img.freepik.com/free-photo/the-girl-in-the-sky_1340-27755.jpg'
+    //         },
+    //         {
+    //             id: v1(), name: 'Alex', followed: false, status: 'I\'am man ',
+    //             location: {
+    //                 cityName: 'Moscow',
+    //                 country: 'Russia'
+    //             },
+    //             photo: 'https://avatars.mds.yandex.net/i?id=1734dc3ff9417d6092956f5da41811a1-4416220-images-thumbs&n=13'
+    //         }
+    //     ])
+    // }, [])
 
     return (
         <div>
             {props.users.map(item => {
                 return <div key={item.id} className={u.appearance}>
-                    <img src={item.photo} className={u.img}/>
+                    {/*<img src={item.photo} className={u.img}/>*/}
                     {item.followed
                         ? <button onClick={() => props.follow(item.id)}>follow</button>
                         : <button onClick={() => props.unFollow(item.id)}>unFollow</button>}
