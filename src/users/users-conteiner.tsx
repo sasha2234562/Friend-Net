fd5583bb78645edb586698427f4623e49b39e107
Sasha <skalitsenia@mail.ru>
2023-08-19 00:03:29 +0300

    mapDispatchToProps лайф-хак

--- src/users/users-conteiner.tsx
+++ src/users/users-conteiner.tsx
@@ -1,6 +1,5 @@
 import {connect} from "react-redux";
 import {AppStateType} from "../redux/redux-store";
-import {Dispatch} from "redux";
 import {
     setCurrentPageAC,
     followAC,
@@ -35,12 +34,12 @@ type StatePropsType = {
 }
 
 export    type MapDispatchToPropsType = {
-    follow: (userID: number) => void;
-    unFollow: (userId: number) => void;
-    setUsers: (users: Array<UserType>) => void;
-    setCurrentPage: (page: number) => void;
-    setTotalCount: (count: number) => void
-    setPreloader: (preloader: boolean) => void
+    followAC : (userID: number)=> void
+    unFollowAC: (userId: number)=> void
+    setUsersAC: (users: Array<UserType>)=> void
+    setCurrentPageAC: (page: number)=> void
+    setPreloaderAC:  (preloader: boolean)=> void
+    setTotalCountAC: (count: number)=> void
 }
 
 type PropsType = StatePropsType & MapDispatchToPropsType;
@@ -49,24 +48,22 @@ class UsersContainerAPI extends React.Component<PropsType> {
 
 
     componentDidMount() {
-        this.props.setPreloader(true)
+this.props.setPreloaderAC(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
-                this.props.setPreloader(false)
-                this.props.setUsers(response.data.items)
-                // this.props.setTotalCount(response.data.totalCount)
-                console.log(response.data)
+                this.props.setPreloaderAC(false)
+                this.props.setUsersAC(response.data.items)
             });
     }
 
 
     onPageChanged = (page: number) => {
-        this.props.setPreloader(true)
-        this.props.setCurrentPage(page)
+        this.props.setPreloaderAC(true)
+        this.props.setCurrentPageAC(page)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
             .then(response => {
-                this.props.setPreloader(false)
-                this.props.setUsers(response.data.items)
+                this.props.setPreloaderAC(false)
+                this.props.setUsersAC(response.data.items)
             });
     }
 
@@ -78,8 +75,8 @@ class UsersContainerAPI extends React.Component<PropsType> {
             users={this.props.users}
             pageSize={this.props.pageSize}
             onPageChanged={this.onPageChanged}
-            follow={this.props.follow}
-            unFollow={this.props.unFollow}
+            follow={this.props.followAC}
+            unFollow={this.props.unFollowAC}
             preloader={this.props.preloader}
         />
     }
@@ -95,27 +92,4 @@ const mapStateToProps = (state: AppStateType) => {
         preloader: state.users.preloader
     }
 }
-const mapDispatchToProps = (dispatch: Dispatch) => {
-    return {
-        follow: (userID: number) => {
-            dispatch(followAC(userID))
-        },
-        unFollow: (userId: number) => {
-            dispatch(unFollowAC(userId))
-        },
-        setUsers: (users: UserType[]) => {
-            dispatch(setUsersAC(users))
-        },
-        setCurrentPage: (page: number) => {
-            dispatch(setCurrentPageAC(page))
-        },
-        setPreloader: (preloader: boolean) => {
-            dispatch(setPreloaderAC(preloader))
-        },
-        setTotalCount: (count: number) => {
-            dispatch(setTotalCountAC(count))
-        }
-    }
-}
-
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainerAPI)
+export const UsersContainer = connect(mapStateToProps, {followAC, unFollowAC, setUsersAC, setCurrentPageAC, setPreloaderAC, setTotalCountAC})(UsersContainerAPI)
