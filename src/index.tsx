91ea8c028351e3676d0c66bbdddd67ed951e2c7a
Sasha <skalitsenia@mail.ru>
2023-10-02 14:28:27 +0300

    90 - redux-ducks, refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
-import App from "./App";
+import App from "./App.js";
 import {store} from "./redux/redux-store"
 import React from "react";
 import {Provider} from "react-redux";
@@ -19,37 +19,3 @@ export const rerenderEntireTree = () => {
 rerenderEntireTree()
 
 store.subscribe(rerenderEntireTree)
-// import {Provider} from "./store-context";
-
-// export type storeType = {
-//     state: {
-//         dialogsPage: DialogsPageType,
-//         profilePage: ProfilePage
-//     }
-//     dispatch: (action: { newText: string; type: string }) => void
-// }
-// type ActionType = ActionNewPostType | ActionNewText | ActionNewMessage
-// type DialogsPageType = {
-//     dialogsName: Array<DialogsNameType>
-//     messages: Array<MessagesType>
-//     newMessage: string
-// }
-// export type  DialogsNameType = {
-//     src: string
-//     name: string
-//     id: string
-// }
-// type MessagesType = {
-//     value: string
-//     id: string
-// }
-//
-// type ProfilePage = {
-//     posts: Array<PostsType>
-//     newPostText: string
-// }
-// type PostsType = {
-//     comment: string
-//     img: string
-// }
-
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,7 +3,7 @@ import {ProfileActionType, profileReducer} from "./profile-reducer";
 import {MessageActionType, messageReducer} from "./message-reduser";
 import {UsersActionType, usersReduser} from "./user-reduser";
 import {AuthLoginType, authReducer} from "./auth-reduser";
-import thunk from "redux-thunk";
+import thunk, {ThunkAction} from "redux-thunk";
 import {FormAction, reducer as formReducer} from 'redux-form';
 import {AppActionsType, appReduser} from "./app-reduser";
 
@@ -17,8 +17,8 @@ let reducers = combineReducers({
     app: appReduser
 })
 
+export type AppThunk<ReturnType = void> = ThunkAction<void, AppStoreType, unknown, any>
 export type AppStoreType = ReturnType<typeof reducers>
-
 export type ActionsAllType = AuthLoginType
     | MessageActionType
     | ProfileActionType
